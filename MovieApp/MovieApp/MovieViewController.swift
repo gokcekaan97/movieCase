7dc92c7834956bd5cb748102c7c0df6d2689094a
Kaan GÖKÇEK <kaan.gokcek@halkbank.com.tr>
2023-11-07 18:03:11 +0300

    snapkit added and ios 11 is covered

---
+++ MovieApp/MovieApp/MovieViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by kaan gokcek on 7.11.2023.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .orange
+  }
+
+
+}
+
