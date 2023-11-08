f77d4faf6a0f9b7c044d422c09cb6a203dd0957a
Kaan GÖKÇEK <kaan.gokcek@halkbank.com.tr>
2023-11-08 14:53:51 +0300

    demo collectionview

--- MovieApp/MovieApp/Scene/MovieViewController.swift
+++ MovieApp/MovieApp/Scene/MovieViewController.swift
@@ -10,12 +10,29 @@ import UIKit
 class MovieViewController: UIViewController {
   
   public var verticalTableView = UITableView()
+  public var horizontalCollectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    layout.scrollDirection = .horizontal
+    let width = UIScreen.main.bounds.width/3
+    layout.itemSize = CGSize(width: width, height: width)
+    layout.sectionInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    return collectionView
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     setupSubviews()
     setupConstraints()
     setupTableView()
+    setupCollectionView()
+  }
+  
+  func setupCollectionView() {
+    horizontalCollectionView.delegate = self
+    horizontalCollectionView.dataSource = self
+    horizontalCollectionView.register(HorizontalMovieCell.self, forCellWithReuseIdentifier: "HorizontalMovieCell")
   }
   
   func setupTableView() {
@@ -26,11 +43,17 @@ class MovieViewController: UIViewController {
 
   func setupSubviews() {
     self.view.addSubview(verticalTableView)
+    self.view.addSubview(horizontalCollectionView)
   }
   
   func setupConstraints() {
     verticalTableView.snp.makeConstraints { make in
-      make.edges.equalToSuperview()
+      make.leading.top.trailing.equalToSuperview()
+      make.bottom.equalTo(horizontalCollectionView.snp.top)
+    }
+    horizontalCollectionView.snp.makeConstraints { make in
+      make.leading.bottom.trailing.equalToSuperview()
+      make.height.equalTo(UIScreen.main.bounds.height/4)
     }
   }
 }
@@ -46,12 +69,32 @@ extension MovieViewController: UITableViewDelegate, UITableViewDataSource {
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as? VerticalMovieCell else {fatalError()}
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? VerticalMovieCell else {fatalError()}
     cell.cellName.text = "demo"
     cell.cellDescription.text = "demo 1"
     cell.image.image = .none
     return cell
   }
+}
+
+//MARK: CollectionView delegate & datasource
+extension MovieViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//    let width = UIScreen.main.bounds.width / 2
+//    return CGSize(
+//      width: width,
+//      height: width
+//    )
+//  }
   
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HorizontalMovieCell", for: indexPath) as? HorizontalMovieCell else {fatalError()}
+    cell.image.image = .none
+    return cell
+  }
   
 }
