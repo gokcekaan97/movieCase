35dd7f326961b01ae8ea5d4257a584759a18c7fd
Kaan GÖKÇEK <kaan.gokcek@halkbank.com.tr>
2023-11-10 17:06:24 +0300

    little indentation

--- MovieApp/MovieApp/Service/Service.swift
+++ MovieApp/MovieApp/Service/Service.swift
@@ -20,10 +20,11 @@ final class ApiService {
     case decodeError
   }
   
-  public func execute<T: Codable>(_ request: ApiRequest,
-                                  expecting type: T.Type,
-                                  completion: @escaping (Result<T,Error>) -> Void ){
-    
+  public func execute<T: Codable>(
+    _ request: ApiRequest,
+    expecting type: T.Type,
+    completion: @escaping (Result<T,Error>) -> Void
+  ){
     guard let url = request.url else { return completion(.failure(ApiServiceError.urlNil)) }
     URLSession.shared.dataTask(with: url){ data, response, error in
       guard let data = data, error == nil else {
